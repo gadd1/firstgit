d70350d8fa52589e778ce2a94b5a59ce16d6f4bd
gadd1 <gad_d@hotmail.com>
2017-01-19 14:39:46 +0200

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -7,13 +7,3 @@ NC='\033[0m' # No Color
 
 echo -e "${red}>>> Retriving latest FW files${NC}"
 sh 6_update_pack.sh
-echo -e "${red}>>> Packing rootfs${NC}"
-sh 7_pack_rootfs.sh
-echo -e "${red}>>> Building ISO${NC}"
-sh 8_generate_iso.sh
-echo -e "${red}>>> Copying ISO${NC}"
-cp mlnx_fw.iso /.autodirect/LIT/OOB/FW_GA/mlnx_fw.iso
-
-echo ""
-echo -e "${green}>>> ISO avilable @ /.autodirect/LIT/OOB/FW_GA/mlnx_fw.iso${NC}"
-echo ""
